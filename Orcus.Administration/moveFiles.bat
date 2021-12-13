c52abc8752a1a7652f4dd97497d1e3d9318cf061
Teal'c <73314940+kgnfth@users.noreply.github.com>
2021-12-13 18:33:29 +0100

    Cleanup release build
    Makes the release folder more cleaner

--- Orcus.Administration/moveFiles.bat
+++ Orcus.Administration/moveFiles.bat
@@ -1,10 +1,22 @@
 @echo off
 if not exist "libraries" mkdir libraries
-
-move de libraries\
+if exist "cs-CZ" rmdir /Q /S "cs-CZ"
+if exist "de" rmdir /Q /S "de"
+if exist "es" rmdir /Q /S "es"
+if exist "fr" rmdir /Q /S "fr"
+if exist "hu" rmdir /Q /S "hu"
+if exist "it" rmdir /Q /S "it"
+if exist "ja-JP" rmdir /Q /S "ja-JP"
+if exist "pt-BR" rmdir /Q /S "pt-BR"
+if exist "ro" rmdir /Q /S "ro"
+if exist "ru" rmdir /Q /S "ru"
+if exist "sv" rmdir /Q /S "sv"
+if exist "zh-Hans" rmdir /Q /S "zh-Hans"
+if exist "Orcus.Administration.pdb" del "Orcus.Administration.pdb"
 move *.dll libraries
 move *.pdb libraries
-move libraries\Orcus.Administration.pdb Orcus.Administration.pdb
 move *.xml libraries
 move *.config libraries
 move libraries\Orcus.Administration.exe.config Orcus.Administration.exe.config
+move libraries\Orcus.Administration.exe.config Orcus.Administration.exe.config
+attrib +h "Orcus.Administration.exe.config"
